4b9ff32f30cbba48253133073a4c019e3af0dd67
Sven Zhang <zhangss@ucalgary.ca>
2017-03-08 22:42:20 -0700

    Added nickname change command. Changed server message color to red

--- server.js
+++ server.js
@@ -1,125 +1,176 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-var clients = new Map();
 var charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-var chatHistory = [];
 var io = require('socket.io').listen(app.listen(port));
 
-app.set('views', __dirname +'/pug');
+app.set('views', __dirname + '/pug');
 app.set('view engine', "pug");
 app.engine('pug', require('pug').__express);
 app.use(express.static(__dirname + '/webClient'));
 
-app.get("/", function( req, res ) {
+var clients = new Map();
+var currentUsers = [];
+var chatHistory = [];
+
+app.get("/", function (req, res) {
 	res.render("page");
 });
 
-io.sockets.on('connect', function(socket) {
+io.sockets.on('connect', function (socket) {
 	var name = generateUsername();
-	clients.set(name, socket);
-	
-	socket.emit('serverMessage', { 
-		timestamp: getTimestamp(),
-		message: 'Welcome to the chat! You have been auto-assigned the username: ' + name + ".",
-		autoUsername: name,
-		userList: generateUserList()
+	clients.set( socket, {
+		username: name,
+		color: '#000'
 	});
 	
+	generateUserList();
+
+	socket.emit('serverMessage', {
+		timestamp: getTimestamp(),
+		message: 'Welcome to the chat! You have been auto-assigned the username: ' + name + ".",
+		username: name,
+		userList: currentUsers
+	});
+
 	socket.broadcast.emit('serverMessage', {
 		timestamp: getTimestamp(),
 		message: '<i>' + name + '</i> has joined the room.',
-		userList: generateUserList()
+		userList: currentUsers
 	});
-	
+
 	console.log("User connected:" + name);
 });
 
-io.sockets.on('connection', function(socket) {
-	socket.on('message', function(data) {
-		console.log( "message received:" + data.message );
-		switch ( parseMessage(data.message) ) {
-			case 0:	//bad server command
-				break;
-			case 1:	//server command
-				break;
-			case 2:	//regular message
-				chatHistory.push( data.message );
-				data.timestamp = getTimestamp();
-				console.log( "sending message: " + data.message );
-				io.sockets.emit('message', data);
-				break;
+io.sockets.on('connection', function (socket) {
+	socket.on('message', function (data) {
+		console.log("Message received: '" + data.message + "' from: " + data.username );
+		if ( data.message.startsWith('/') ) {
+			handleServerCommand( socket, data.message );
+		} else {
+			chatHistory.push(data.message);
+			data.timestamp = getTimestamp();
+			console.log("Broadcasting message: " + data.message);
+			io.sockets.emit('message', data);
 		}
 	});
-});
-
-io.sockets.on('disconnect', function() {
-	let deadUser = '';
-		for ( let [userId, connection] of clients ) {
-			if ( socket == connection ) {
-				deadUser = userId;
-				clients.delete(userId);
-				console.log("User disconnected: " + deadUser);
-				break;
-			}
-		}
 	
-	io.sockets.emit('serverMessage', {
-		timestamp: getTimestamp(),
-		message: '<i>' + deadUser + '</i> has left the room.',
-		userList: generateUserList()
+	socket.on('disconnect', function() {
+		let deadUser = clients.get(socket).username;
+	
+		if ( !clients.has(socket) ) {
+			console.log("Attempted to disconnect a user that was not logged...?");
+			return;
+		}
+		
+		clients.delete(socket);
+		console.log("User disconnected:" + deadUser);
+		
+		generateUserList();
+		
+		io.sockets.emit('serverMessage', {
+			timestamp: getTimestamp(),
+			message: '<i>' + deadUser + '</i> has left the room.',
+			userList: currentUsers
+		});
 	});
 });
 
 getTimestamp = function () {
 	let date = new Date();
 	let hh = date.getHours();
-	if ( hh < 10 )
+	if (hh < 10)
 		hh = '0' + hh;
-	
+
 	let mm = date.getMinutes();
-	if ( mm < 10 )
+	if (mm < 10)
 		mm = '0' + mm;
-	
+
 	let ss = date.getSeconds();
-	if ( ss < 10 )
+	if (ss < 10)
 		ss = '0' + ss;
-	
+
 	return hh + ":" + mm + ":" + ss;
 };
 
 generateUsername = function () {
-    let text = "";
+	let text = "";
 
-    for( var i=0; i < 5; i++ )
-        text += charSet.charAt(Math.floor(Math.random() * charSet.length));
-
-	while( clients.has(text) )	
-		text = generateUsername(); //possible dangerous recursion? seems unlikely
+	for (var i = 0; i < 7; i++)
+		text += charSet.charAt(Math.floor(Math.random() * charSet.length));
 	
-    return text;
+	while (clients.has(text))
+		text = generateUsername(); //possible dangerous recursion? seems unlikely
+
+	return text;
 };
 
 generateUserList = function () {
-	let userList = [];
-	for ( let[userId, socket] of clients )
-		userList.push(userId);
+	currentUsers = [];
 	
-	return userList.sort();
+	for (let[socket, info] of clients)
+		currentUsers.push(info.username);
+
+	currentUsers.sort();
 };
 
-parseMessage = function( message ) {
-	if ( message.match('^\/') ) {
-		let tokens = message.split(" ");
-		switch( tokens[0] ) {
-			case '/nick':
-				break;
-			case '/nickcolor':
-				break;
-			default:
-				return 0;
+handleServerCommand = function ( socket, message ) {
+	console.log("Handling server command: " + message );
+	let tokens = message.split(' ');
+	switch ( tokens[0].toLowerCase() ) {
+		case '/nick':
+			handleChangeNickname(socket, tokens);			
+			break;
+		case '/nickcolor':
+			handleChangeNickColor(socket, tokens);
+			break;
+		default:
+			console.log("badCommand: " + message );
+			socket.emit('serverMessage', {
+				timestamp: getTimestamp(),
+				message: "What? I didn't understand that command. <br>Currently supported commands: <br>'/nick' <br>'/nickcolor'"
+			});
+	}
+};
+
+handleChangeNickname = function( socket, tokens ) {
+	if ( tokens.length < 2 ) {
+		console.log("No new nickname supplied");
+		socket.emit('serverMessage', {
+			timestamp: getTimestamp(),
+			message: "You didn't supply a new username!"
+		});
+	} else {
+		let userInfo = clients.get(socket);
+		let oldName = userInfo.username;
+		if ( /[\W]/.test( tokens[1] ) || tokens[1].trim().length === 0 ) {
+			console.log("Bad nickname change request: " + tokens[1]);
+			socket.emit('serverMessage', {
+				timestamp: getTimestamp(),
+				message: "Your nickname must contain only alphanumeric characters"
+			});
+		} else {
+			userInfo.username = tokens[1];
+			socket.emit('serverMessage', {
+				timestamp: getTimestamp(),
+				username: tokens[1],
+				message: "Successfully changed nickname to " + tokens[1]
+			});
+			
+			generateUserList();
+			
+			io.sockets.emit('serverMessage', {
+				timestamp: getTimestamp(),
+				message: '<i>' + oldName + '</i> is now known as <i>' + userInfo.username + '</i>',
+				userList: currentUsers
+			});
+			console.log("Setting nickname for " + oldName + " to " + tokens[1]);
 		}
 	}
-	return 2;
 };
+
+handleChangeNickColor = function( socket, tokens ) {
+	
+}
+
 console.log("Listening on port " + port);
--- webClient/client.js
+++ webClient/client.js
@@ -1,38 +1,46 @@
-$(function() {
-	var socket = io();
-	var username = "";
-	var userList = [];
-	
+var socket = io();
+var myName = "";
+var userList = [];
+
+$(function () {
 	sendMessage = function () {
-		socket.emit('message', { username: username, message: $('#textField').val() });
+		socket.emit('message', {
+			username: myName,
+			message: $('#textField').val()
+		});
 		$('#textField').val('');
 	};
-	
-	$("#textField").keyup(function(e) {
-		if(e.keyCode == 13) {
+
+	$("#textField").keyup(function (e) {
+		if (e.keyCode == 13) {
 			sendMessage();
 		}
 	});
-	
-	$("#sendButton").click(function() {
+
+	$("#sendButton").click(function () {
 		sendMessage();
 	});
-	
-	buildMessageString = function ( data ) {
-		return '<b><i>' + data.timestamp + '</i> | ' + (data.username ? data.username : 'Server') + ": </b>" + data.message;
+
+	buildMessageString = function (data) {
+		return '<b><i>' + data.timestamp + '</i> | ' + data.username + ": </b>" + data.message;
 	}
-	
-	socket.on('message', function( data ) {
-		$('#messageList').append($('<li>').html( buildMessageString( data ) ) );
+
+	socket.on('message', function (data) {
+		$('#messageList').append($('<li>').html(buildMessageString(data)));
 	})
-	
-	socket.on('serverMessage', function( data ) {
-		if ( data.autoUsername )
-			username = data.autoUsername;
-		if ( data.userList )
-			userList = data.userList;
-		if ( data.message )
-			$('#messageList').append($('<li>').html( buildMessageString( data ) ) );
-	})
-	
+
+	socket.on('serverMessage', function (data) {
+		handleServerMessage(data);
+	});
 });
+
+handleServerMessage = function (data) {
+	if (data.userList) 
+		userList = data.userList;
+	if (data.username) 
+		myName = data.username;		
+	if (data.message) {
+		data.username = 'Server'
+		$('#messageList').append($('<li>').html(buildMessageString(data))).css("color", "red");
+	}
+}
